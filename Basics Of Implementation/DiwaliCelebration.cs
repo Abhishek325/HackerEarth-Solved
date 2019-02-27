f80641284e98667738939e513d58ddae440c5604
Abhishek Kumar Singh <abhishek.singh@inszoom.com>
2019-02-28 00:20:33 +0530

    Diwali Celebration - solved

---
+++ Basics Of Implementation/DiwaliCelebration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class DiwaliCelebration
+{
+    public DiwaliCelebration()
+    {
+        int noOfTestCases = Convert.ToInt16(Console.ReadLine());
+        while (noOfTestCases-- > 0)
+        {
+            string input = Console.ReadLine();
+            string[] temp = input.Split(' ');
+            int a = Convert.ToInt16(temp[0]);
+            int k = Convert.ToInt16(temp[1]);
+            int n = Convert.ToInt16(temp[2]);
+
+            Console.WriteLine(a + (n - 1) * k);
+        }
+    }
+}
